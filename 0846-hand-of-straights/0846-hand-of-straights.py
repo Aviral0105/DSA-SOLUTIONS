860195d247a937922b568853ed8c8bd58e4256df
AVIRAL JAIN <aviral01jain@gmail.com>
2026-08-08 16:41:27 +0530

    Time: 26 ms (89.4%), Space: 21.4 MB (17.83%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        count=Counter(hand)
+
+        minheap=list(count.keys())
+        heapq.heapify(minheap)
+
+        while minheap:
+            first=minheap[0]
+
+            if count[first]==0:
+                heapq.heappop(minheap)
+                continue
+            for i in range(first,first+groupSize):
+                if count[i]==0:
+                    return False
+                count[i]-=1
+        return True
